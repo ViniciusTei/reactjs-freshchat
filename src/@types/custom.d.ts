b593701bea71954b1c85757df2abea93ce0ca99f
Vincius <viniciustprates@gmail.com>
2021-04-28 14:01:52 -0300

    🏷 add type modules

--- src/@types/custom.d.ts
+++ src/@types/custom.d.ts
@@ -3,5 +3,20 @@ declare module "*.svg" {
   export default content;
 }
 
+declare module 'reactjs-freshchat' {
+  interface FreshchatStyles {
+    backgroundColor: string;
+    color: string;
+  }
+  
+  export interface  FreshChatProps {
+    token: string;
+    externalId?: string;
+    firstName?: string;
+    lastName?: string;
+    label?: string;
+    styles?: FreshchatStyles;
+  }
+}
 
 
