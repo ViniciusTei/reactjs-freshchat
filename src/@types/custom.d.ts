d3d1d4317a5789ebd5f1a36ccb2b43950a8e98ed
Vincius <viniciustprates@gmail.com>
2021-04-28 13:53:53 -0300

    :label: Export type definitions

--- src/@types/custom.d.ts
+++ src/@types/custom.d.ts
@@ -3,16 +3,5 @@ declare module "*.svg" {
   export default content;
 }
 
-declare type FreshchatStyles = {
-  backgroundColor: string;
-  color: string;
-}
 
-declare type  FreshChatProps = {
-  token: string;
-  externalId?: string;
-  firstName?: string;
-  lastName?: string;
-  label?: string;
-  styles?: FreshchatStyles;
-}
+
