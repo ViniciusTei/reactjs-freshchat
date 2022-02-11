6f474070119a8d37cdda0c0bbe06b165fe0fe552
ViniciusTei <viniciustprates@gmail.com>
2022-02-11 15:48:46 -0300

    add custom styles tests

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -4,12 +4,35 @@ import { Freshchat } from './index'
 
 describe('Freshchat', () => {
   it('should render correctly', () => {
-    expect(<Freshchat token='fake-api-token' />).toBeTruthy()
+    const rendred = render(<Freshchat token='fake-api-token' />)
+    expect(rendred.container.firstChild).toBeNull()
   })
 
   it('should render with label', () => {
     const rendred = render(
-      <Freshchat token='fake-api-token' label='Label text' />
+      <Freshchat
+        token='fake-api-token'
+        label='Label text'
+        config={{}}
+        host='fake-host'
+      />
+    )
+
+    expect(rendred.getByText('Label text')).toBeInTheDocument()
+  })
+
+  it('it should render with label and custom styles', () => {
+    const rendred = render(
+      <Freshchat
+        token='fake-api-token'
+        label='Label text'
+        config={{}}
+        host={undefined}
+        ic_styles={{
+          backgroundColor: '#002d85',
+          color: '#ffffff'
+        }}
+      />
     )
 
     expect(rendred.getByText('Label text')).toBeInTheDocument()
